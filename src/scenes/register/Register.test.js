53c88988cc065cb09483e8f2ce1d696e5910d842
Cesario <danieltc@ciandt.com>
2019-10-31 10:25:33 -0300

    Dojo

--- src/scenes/register/Register.test.js
+++ src/scenes/register/Register.test.js
@@ -5,6 +5,7 @@ import Register from './Register'
 
 describe('Render', () => {
   const wrapper = shallow(<Register />)
+  
 
   it('Should render a correct title', () => {
     expect(wrapper.find('.page-title')).toHaveLength(1)
@@ -12,9 +13,23 @@ describe('Render', () => {
   })
 
   it('Should render a correct fields', () => {
+    expect(wrapper.find('#person-document')).toHaveLength(1)
+    expect(wrapper.find('#person-name')).toHaveLength(1)
+    expect(wrapper.find('#person-city')).toHaveLength(1)
+    expect(wrapper.find('#person-age')).toHaveLength(1)
+
+    expect(wrapper.find('#person-document').prop('label')).toEqual('Document')
+    expect(wrapper.find('#person-name').prop('label')).toEqual('Name')
+    expect(wrapper.find('#person-city').prop('label')).toEqual('City')
+    expect(wrapper.find('#person-age').prop('label')).toEqual('Age')
   })
 
   it('Should render a correct buttons', () => {
+    expect(wrapper.find('#save-button')).toHaveLength(1)
+    expect(wrapper.find('#cancel-button')).toHaveLength(1)
+
+    expect(wrapper.find('#save-button').text()).toEqual('Register')
+    expect(wrapper.find('#cancel-button').text()).toEqual('Cancel')
   })
 })
 
@@ -32,7 +47,6 @@ describe('Interactions', () => {
   const wrapper = shallow(<Register />)
 
   it('Should save a person when click Register Button', () => {
-
     // Mock Service response
     const firebaseService = require('../../services/FirebaseService')
     jest
@@ -53,6 +67,18 @@ describe('Interactions', () => {
   })
 
   it('Should clear a form when click Cancel Button', () => {
+    fillField(wrapper, '#person-document', '123')
+    fillField(wrapper, '#person-name', 'abc')
+    fillField(wrapper, '#person-city', 'cde')
+    fillField(wrapper, '#person-age', '20')
+
+    wrapper.find('#cancel-button').simulate('click')
+
+    expect(wrapper.find('#person-document').prop('value')).toEqual('')
+    expect(wrapper.find('#person-name').prop('value')).toEqual('')
+    expect(wrapper.find('#person-city').prop('value')).toEqual('')
+    expect(wrapper.find('#person-age').prop('value')).toEqual('')
+  
   })
 })
 
